598be4df159885a3e77bcb53558a38465e443dce
Seth Nelson <seth.nelson@outlook.com>
2020-02-18 20:31:55 -0800

    finished up the home link button and got assistance with fixing my syntax

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -15,10 +15,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path='/' component={MovieList} />\
+      <Route exact path='/' component={MovieList} />
       {/* <Route path='/movieList/' render={(props) => <MovieList {...props} savedList={savedList} /> } /> */}
       {/* props above in movie component? */}
-      <Route path ='/Movies/:id' render={(props) => <Movie {...props} addToSavedList={addToSavedList} /> } />
+      <Route path='/movies/:id' render={(props) => <Movie {...props} addToSavedList={addToSavedList} /> } />
     </div>
   );
 };
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const MovieCard = props => {
-  return;
+  const {movie} = props 
+  //this acts as destructuring for the props (instead of props.movie.title, etc)
+  return (
+    <div className="movie-card">
+      <h2>{movie.title}</h2>
+      <div className="movie-director">
+        Director: <em>{movie.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{movie.metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {movie.stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  )
 };
 
 export default MovieCard;
